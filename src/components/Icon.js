a15d8e56bbb44cb377c1ef03fbfa2ceb9981a36f
javo787 <user.dr04@gmail.com>
2026-02-04 02:41:21 +0500

    search icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -144,11 +144,11 @@ export default function Icon({ name, className = '', ...props }) {
     ),
    
    search: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" className={className} {...props}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" className={className} {...props}>
         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
       </svg>
-    ), 
-
+    ),
+    
     sort: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
